c897c59af7d560e80043de842ec939805a63e3d6
Boris Wilhelms <mail@wille-zone.de>
2014-09-15 22:56:11 +0200

    REFACTORED: Refactored extractDate and extractLastChange (DRY)

--- Substitution/extractDate.go
+++ Substitution/extractDate.go
@@ -6,22 +6,21 @@ import (
 )
 
 func extractDate(s string) (time.Time, error) {
-	r, _ := regexp.Compile("<div class=\"mon_title\">(\\d{2}.\\d?\\d.\\d{4}) \\w+</div>")
-	m := r.FindAllStringSubmatch(s, -1)
-	date, err := time.Parse("02.1.2006", m[0][1])
-
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	return date, nil
+	return extractTime(s,
+		"<div class=\"mon_title\">(\\d{2}.\\d?\\d.\\d{4}) \\w+</div>",
+		"02.1.2006")
 }
 
 func extractLastChange(s string) (time.Time, error) {
-	r, _ := regexp.Compile("Stand: (\\d{2}.\\d{2}.\\d{4} \\d{2}:\\d{2})")
+	return extractTime(s, "Stand: (\\d{2}.\\d{2}.\\d{4} \\d{2}:\\d{2})",
+		"02.1.2006 15:04")
+}
+
+func extractTime(s string, rexp string, format string) (time.Time, error) {
+	r, _ := regexp.Compile(rexp)
 
 	m := r.FindAllStringSubmatch(s, -1)
-	date, err := time.Parse("02.1.2006 15:04", m[0][1])
+	date, err := time.Parse(format, m[0][1])
 
 	if err != nil {
 		return time.Time{}, err
